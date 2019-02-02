a77be1620acf67331cd3f3acf641f13d36b2efb1
Chris Wolfe <chriswwolfe@gmail.com>
2019-02-02 10:08:58 -0800

    remove test

--- db/db_test.go
+++ db/db_test.go
@@ -39,25 +39,3 @@ func TestQueryResults(t *testing.T) {
 		fmt.Printf("%v\n", result)
 	}
 }
-
-func TestRegressions(t *testing.T) {
-	db, err := BuildDB("../results.db")
-	defer db.Close()
-
-	assert.Nil(t, err, "failed to build db")
-
-	// this relies on data in the DB!
-	lifters, err := db.QueryNames("jessie b")
-	assert.Nil(t, err, "query for names returned an error")
-	assert.NotEmpty(t, lifters, "no names returned")
-
-	summary, err := db.QueryResults(lifters[0].Name, lifters[0].Hometown)
-	assert.Nil(t, err, "query for results failed")
-
-	assert.NotEmpty(t, summary.Results, "no results for lifter")
-	for _, result := range summary.Results {
-		// let's make sure we have what we expect
-		fmt.Printf("%v\n", result)
-	}
-}
-
