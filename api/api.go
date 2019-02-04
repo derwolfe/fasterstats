8b47b9d93169a943a32e98c2c3dbce7f2ce32367
Chris Wolfe <chriswwolfe@gmail.com>
2019-02-03 19:39:14 -0800

    comment out css

--- api/api.go
+++ api/api.go
@@ -106,7 +106,8 @@ func (a API) Results(w http.ResponseWriter, r *http.Request) {
 }
 
 var css = `{{ define "css" }}
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+{{/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+*/}}
 {{ end }}`
 
 var searchNamesResults = `{{ define "content" }}<div>
