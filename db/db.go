7470444841f2ff40c9e3de609cfb31534b2642c9
Chris Wolfe <chriswwolfe@gmail.com>
2019-02-15 21:45:04 -0800

    logging

--- db/db.go
+++ db/db.go
@@ -141,7 +141,7 @@ type LiftersResponse struct {
 }
 
 func (o *OurDB) QueryNames(name, offset string) (*LiftersResponse, error) {
-	log.Printf("name: %v\n", name)
+	log.Printf("name: %v, offset: %v\n", name, offset)
 	name = strings.Replace(name, " ", "%", -1)
 	nameLike := "%" + name + "%"
 
